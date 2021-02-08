07ec2142b9a6eaf3a25f5a4c39570fd49e22eadd
nicko <nicko.lee@hotmail.com>
2021-02-08 09:08:58 +0000

    Add some test data

--- Code/demo-pw-hashing.py
+++ Code/demo-pw-hashing.py
@@ -1,17 +1,19 @@
 import sys
+import time
 from hashlib import sha256
 
 # Prove app has started
 print("Hello World")
 
-
+# Source: https://stackoverflow.com/questions/60241267/how-to-hash-sha256-many-times-in-python
 pw = input('Enter Password: ')
 h = sha256(pw.encode('utf-8')).digest()
 
-N = 100
+N = 1000
 for i in range(N):
     if i != N-1:
         h = sha256(h).digest()
+        time.sleep(0.01) ## in order for people to see the steps else it is too fast
         print(f"Iteration #{i+1}: {sha256(h).hexdigest()}")  
 
     else:
---
+++ Code/demo-pw-list-encryption.py
@@ -0,0 +1,13 @@
+
+pw_list = open("sample_data.csv", "r")
+
+# Before encryption
+print("BEFORE ENCRYPTION")
+print("-----------------\n")
+for line in pw_list:
+  print(line)
+
+# After encryption
+print("\n")
+print("AFTER ENCRYPTION")
+print("----------------\n")
